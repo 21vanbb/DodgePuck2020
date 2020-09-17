150c0336c917bed8689cdf31e3627b5c0c36504f
21vanbb <21vanbb@live.elida.k12.oh.us>
2020-09-17 09:16:09 -0400

    Fourth Commit

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
+    public int score = 7;
+    public int highscore = 10;
+    private int temp = 70;
+    public float location = 0.0f;
+    public float loc2 = 1.5f; 
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello, World!"); 
+        //Debug.Log("Hello, World!");
+        score += 2; 
+        Debug.Log(score + highscore);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("D was pressed"); 
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Debug.Log("location: " + 0.5f);
+        }
     }
 }
